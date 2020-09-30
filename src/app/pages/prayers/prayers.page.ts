d0a0c46054c92efaacaaebda9cf8ff790e866069
Cintia <cin.contaadm@gmail.com>
2020-09-29 22:55:13 -0300

    Criação de accordion - página Motivos de Oração.

--- src/app/pages/prayers/prayers.page.ts
+++ src/app/pages/prayers/prayers.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +6,34 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './prayers.page.html',
   styleUrls: ['./prayers.page.scss'],
 })
-export class PrayersPage implements OnInit {
+export class PrayersPage {
 
-  constructor() { }
+  // constructor() {}
 
-  ngOnInit() {
+  // ngOnInit() {
+  // }
+  prayersInfo: any[];
+
+  // automaticClose = false;
+
+  constructor(private http: HttpClient) {
+    this.http.get('assets/prayersInfo.json').subscribe(res => {
+      this.prayersInfo = res['prayers'];
+      this.prayersInfo[0].open = true;
+    });
   }
 
+  toggleSection(index) {
+    this.prayersInfo[index].open = !this.prayersInfo[index].open;
+    console.log(index);
+    if (this.prayersInfo[index].open) {
+      this.prayersInfo
+        .filter((prayer, prayerIndex) => prayerIndex !== index)
+        .map(prayer => prayer.open = false);
+    }
+  }
+
+  // toggleItem(index, detailIndex) {
+  //   this.prayersInfo[index].details[detailIndex].open = !this.prayersInfo[index].details[detailIndex].open;
+  // }
 }
